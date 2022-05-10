880515ade64fda6e42d68997a23c54679c6c8172
Augusto Sousa <lagrange.tesla@gmail.com>
2022-05-09 19:53:28 -0500

    Add crud route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('mascotas');
 });
+
+Route::resource('mascotas', \App\Http\Controllers\MascotaController::class);
